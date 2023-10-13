734411dd8c0f46e3899ff2b02cdeee2871b37f12
Gowtham12345667 <gowthamreddyp47@gmail.com>
2023-10-13 11:32:10 +0530

    Gowtham

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfTwoNumbers
+ {
+    public static void main(String[] args) 
+{
+        double num1 = 5.5;  
+        double num2 = 3.0;
+
+        double sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
